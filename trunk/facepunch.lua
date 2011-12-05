eef73126c10f402946fbbc2624f2de82240d9e3b
mcwattersandrew <mcwattersandrew@0f1eec40-32ba-17e8-742a-c622b1ca65ea>
2011-12-05 17:11:09 +0000

    Small pre-post update

--- trunk/facepunch.lua
+++ trunk/facepunch.lua
@@ -6,7 +6,7 @@ baseURL = "http://www.facepunch.com/"
 rootURL = "http://www.facepunch.com"
 
 -------------------------------------------------------------------------------
--- request()
+-- facepunch.request()
 -- Purpose: The core request function for the facepunch module. All retrieval
 --			functions rely on this wrapper for parsing. It must return the full
 --			page if possible and a status code (200 OK).
