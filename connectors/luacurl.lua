a45479b65885864d9d6fde42b712dee059cfb44a
Andrew McWatters <me@andrewmcwatters.com>
2011-12-16 07:55:23 +0000

    Added libcurl.dll and luacurl.dll to the ignore list Readded luacurl connector handling, threadtest.lua confirms working

--- connectors/luacurl.lua
+++ connectors/luacurl.lua
@@ -5,9 +5,36 @@
 --			Gran PC
 --			Gregor Steiner
 -------------------------------------------------------------------------------
+require( "luacurl" )
+
+local curl = curl
+local string = string
+local table = table
+local tonumber = tonumber
+
+local function curlWrite( bufferTable )
+	return function( stream, buffer )
+		table.insert( bufferTable, buffer )
+		return string.len( buffer )
+	end
+end
 
 function facepunch.http.get( URL )
+	local t = {}
+	local curlObj = curl.new()
+	
+	curlObj:setopt( curl.OPT_HEADER, true )
+	curlObj:setopt( curl.OPT_WRITEFUNCTION, curlWrite( t ) )
+	curlObj:setopt( curl.OPT_URL, URL )
+	
+	curlObj:perform()
+	curlObj:close()
+	
+	local r = table.concat( t, "" )
+	local h, c, m = string.match( r, "(.-) (.-) (.-)\n" )
+	return r, tonumber( c )
 end
 
 function facepunch.http.post( URL, postData )
+	error( "not yet implemented!", 2 )
 end
