761a2958a90b0269e56f1ef69fd02a995ba28139
Andrew McWatters <me@andrewmcwatters.com>
2011-12-16 01:22:30 +0000

    Added placeholder connector helpers

---
+++ connectors/luasocket.lua
@@ -0,0 +1,13 @@
+-------------------------------------------------------------------------------
+-- LuaSocket connector
+-- Facepunch Lua API
+-- Authors: Andrew McWatters
+--			Gran PC
+--			Gregor Steiner
+-------------------------------------------------------------------------------
+
+function facepunch.http.get( URL )
+end
+
+function facepunch.http.post( URL, postData )
+end
