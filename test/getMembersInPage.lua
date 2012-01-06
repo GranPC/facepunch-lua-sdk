f4aea83495b415ab5891782610ace40e92cdcada
Andrew McWatters <me@andrewmcwatters.com>
2012-01-06 04:38:13 +0000

    Updated getMembersInPage.lua to use a session for the example to avoid silently failing

--- test/getMembersInPage.lua
+++ test/getMembersInPage.lua
@@ -2,19 +2,34 @@
 -- usage: lua test\getMembersInPage.lua
 
 local facepunch	= require( "facepunch" )
+local session	= facepunch.session
 local thread	= facepunch.thread
 
 -- Setup our connector
 -- Use luacurl for this test
 require( "connectors.luacurl" )
 
+io.write( "Username: " )
+local username = io.read()
+io.write( "Password: " )
+local password = io.read()
+
+local mySession = session( username, password )
+print( "Logging in as " .. username .. "..." )
+local error = -1
+while error ~= 0 do
+	error = mySession:login()
+end
+
+session.setActiveSession( mySession )
+
 -- Andrew; retrieve a thread, page 1
 local error, threadPage = -1, ""
 while error ~= 0 do
-	error, threadPage = thread.getPage( 1144771, 1 )
+	error, threadPage = thread.getPage( 1151723, 1 )
 end
 
--- Andrew; prints all users who have posted on page 1 of WAYWO December 2011
+-- Andrew; prints all users who have posted on page 1 of WAYWO January 2012
 local members = thread.getMembersInPage( threadPage )
 for _, member in pairs( members ) do
 	print( member )
