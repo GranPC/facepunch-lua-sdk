e8f3a854f447cec6e0a1d90c2c35467bc8b716b3
Gran.PC@gmail.com <Gran.PC@gmail.com@0f1eec40-32ba-17e8-742a-c622b1ca65ea>
2012-01-07 20:29:46 +0000

    Working on the FP monitor: added a scrollbar... some of the code will need to be cleaned up tomorrow.

--- trunk/test/monitor/monitor.lua
+++ trunk/test/monitor/monitor.lua
@@ -16,15 +16,52 @@ Colours[ "Banned" ] = Color.FromArgb( 255, 0, 0 )
 Colours[ "Gold Member" ] = Color.FromArgb( 160, 96, 0 )
 Colours[ "Moderator" ] = Color.FromArgb( 0, 170, 0 )
 
-local TitleHTML = [[<html><style type="text/css">
-.usertitle{white-space:normal;font-size:10px;clear:both;display:block;overflow:hidden;color:#3E3E3E;
-font-family:Tahoma,Helvetica,Arial,Verdana!important;
-line-height:1.230;
-font:13px/1.231 arial,helvetica,clean,sans-serif;}
-</style>~TITLE~</html>]]
+local font = Font( "Segoe UI", 9, FontStyle.Regular, GraphicsUnit.Point, 0 )
+local fontBold = Font( "Segoe UI", 9, FontStyle.Bold, GraphicsUnit.Point, 0 )
 
 local function CleanHTML( str )
 	return string.gsub( str, "%b<>", "" )
+end
+
+local function CreateThread( threadID, name, poster, newPosts )
+	local threadPanel = Panel()
+	local threadNameLabel = LinkLabel()
+	local threadPosterLabel = Label()
+	--local newPostsLabel = LinkLabel()
+
+	threadNameLabel.ActiveLinkColor = Color.FromArgb( 30, 132, 234 )
+	threadNameLabel.AutoSize = true
+	threadNameLabel.DisabledLinkColor = Color.FromArgb( 50, 50, 50 )
+	threadNameLabel.Font = fontBold
+	threadNameLabel.LinkBehavior = LinkBehavior.HoverUnderline
+	threadNameLabel.LinkColor = threadNameLabel.ActiveLinkColor
+	threadNameLabel.Location = Point( 5, 0 )
+	threadNameLabel.Name = "threadNameLabel_"..threadID
+	threadNameLabel.Size = Size( 143, 10 )
+	threadNameLabel.Padding = Padding( 6, 6, 0, 0 )
+	threadNameLabel.Text = name
+	--threadNameLabel.TextAlign = ContentAlignment.MiddleCenter
+	threadNameLabel.VisitedLinkColor = threadNameLabel.LinkColor
+	threadNameLabel.LinkClicked:Add( function()
+	end )
+
+	threadPosterLabel.AutoSize = true
+	threadPosterLabel.Font = font
+	threadPosterLabel.Location = Point( 5, 17 )
+	threadPosterLabel.Name = "threadPosterLabel_"..threadID
+	threadPosterLabel.Padding = Padding( 6, 6, 0, 0 )
+	threadPosterLabel.Size = Size( 143, 21 )
+	threadPosterLabel.TabIndex = 1
+	threadPosterLabel.Text = poster
+
+	threadPanel.Anchor = AnchorStyles.None
+	threadPanel.Controls:Add( threadNameLabel )
+	threadPanel.Controls:Add( threadPosterLabel )
+	threadPanel.Name = "threadPanel_"..threadID
+	threadPanel.Size = Size( 238, 50 )
+	--threadPanel.BackColor = Color.FromArgb(120, 120, 120)
+
+	return threadPanel
 end
 
 function InitializeWindow( uID )
@@ -32,6 +69,10 @@ function InitializeWindow( uID )
 	while err ~= 0 do
 		err, user = member.getDataByUserID( uID )
 	end
+
+	Application.EnableVisualStyles()
+
+	local w, h = 280, 250
 
 	local parent = Form()
 
@@ -41,16 +82,21 @@ function InitializeWindow( uID )
 	local currentlyLoggedInLabel = Label()
 	local avatarPictureBox = PictureBox()
 	local usernameLabel = Label()
-
 	local userTitleLabel = Label()
+
+	-- thread list
+
+	local threadList = Panel()
+	local threadListScroll = VScrollBar()
+
 	controlPanel:SuspendLayout()
 
 	controlPanel.Anchor = AnchorStyles.None
 	controlPanel.BackColor = Color.FromArgb( 241, 245, 251 )
 	controlPanel.BackgroundImageLayout = ImageLayout.None
 	controlPanel.Controls:Add( controlPanelGradient )
-	controlPanel.Controls:Add( openMonitorLink) 
-	controlPanel.Location = Point( 0, 73 )
+	controlPanel.Controls:Add( openMonitorLink )
+	controlPanel.Location = Point( 0, h - 57 )
 	controlPanel.Name = "controlPanel"
 	controlPanel.Size = Size( 264, 41 )
 	controlPanel.TabIndex = 0
@@ -62,9 +108,6 @@ function InitializeWindow( uID )
 	controlPanelGradient.Name = "controlPanelGradient"
 	controlPanelGradient.Size = Size( 264, 4 )
 	controlPanelGradient.TabIndex = 1
-
-	local font = Font( "Segoe UI", 9, FontStyle.Regular, GraphicsUnit.Point, 0 )
-	local fontBold = Font( "Segoe UI", 9, FontStyle.Bold, GraphicsUnit.Point, 0 )
 
 	openMonitorLink.ActiveLinkColor = Color.FromArgb( 0, 102, 204 )
 	openMonitorLink.Anchor = AnchorStyles.Bottom
@@ -123,16 +166,55 @@ function InitializeWindow( uID )
 	controlPanel:ResumeLayout( false )
 	controlPanel:PerformLayout()
 
+	threadList.Anchor = AnchorStyles.None
+--	threadList.BackColor = Color.FromArgb( 241, 35, 251 )
+	threadList.BackgroundImageLayout = ImageLayout.None
+	threadList.Controls:Add( threadListScroll )
+	threadList.Location = Point( 11, 78 )
+	threadList.Name = "threadList"
+	threadList.Size = Size( 238, 110 )
+
+	threadListScroll.Dock = DockStyle.Right
+	threadListScroll.Name = "threadListScroll"
+	threadListScroll.TabIndex = 2
+	threadListScroll.Size = Size( 18, 110 )
+
+	local latestThread = 0
+
+	local threadControls = {}
+
+	for i=1, 5 do
+		local thread = CreateThread( 1, "Thread "..i, "Poster "..i, i*2 )
+		thread.Location = Point( 0, 40 * (i - 1) )
+		threadList.Controls:Add( thread )
+		table.insert( threadControls, thread )
+		latestThread = i
+	end
+
+	if ( latestThread >= 3 ) then
+		threadListScroll.Visible = true
+		threadListScroll.Maximum = ( 5 * (latestThread - 1) )
+		threadListScroll.Value = 0
+	else
+		threadListScroll.Visible = false
+	end
+
+	threadListScroll.ValueChanged:Add( function()
+		for k, v in pairs( threadControls ) do
+			v.Location = Point( 0, 40 * (k - 1) - threadListScroll.Value * 5 )
+		end
+	end )
+
 	parent.AutoScaleDimensions = SizeF( 6, 13 )
 	parent.AutoScaleMode = AutoScaleMode.Font
 	parent.BackColor = SystemColors.Window
-	parent.ClientSize = Size( 280, 130 )
+	parent.ClientSize = Size( w, h )
 	parent.ControlBox = false
 	parent.Cursor = Cursors.Default
 --	parent.DefaultCursor = parent.Cursor
 	parent.MaximizeBox = false
 	parent.MinimizeBox = false
-	parent.MaximumSize = Size( 280, 130 )
+	parent.MaximumSize = parent.ClientSize
 	parent.MinimumSize = parent.MaximumSize
 	parent.Name = "flyoutForm"
 
@@ -141,7 +223,7 @@ function InitializeWindow( uID )
 --	function parent.WndProc(a,b,c,d,e) end
 
 	local bounds = Screen.FromControl( parent ).WorkingArea
-	local pos = Point( bounds.Right - 290, bounds.Bottom - 140 )
+	local pos = Point( bounds.Right - w - 10, bounds.Bottom - h - 10 )
 	parent.Location = pos
 
 	parent.ShowIcon = false
@@ -152,6 +234,7 @@ function InitializeWindow( uID )
 	parent.Controls:Add( currentlyLoggedInLabel )
 	parent.Controls:Add( avatarPictureBox )
 	parent.Controls:Add( controlPanel )
+	parent.Controls:Add( threadList )
 
 	parent.SizeGripStyle = SizeGripStyle.Hide
 	parent.StartPosition = FormStartPosition.Manual
@@ -164,7 +247,7 @@ function InitializeWindow( uID )
 	return parent
 end
 
---InitializeWindow( 175313 ) -- test!
+-- InitializeWindow( 175313 ) -- test!
 
 io.write( "Username: " )
 local username = io.read()
