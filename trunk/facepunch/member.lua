275f28adced9b06cd3945c5e1c43646d86e078c6
mcwattersandrew@gmail.com <mcwattersandrew@gmail.com@0f1eec40-32ba-17e8-742a-c622b1ca65ea>
2012-05-01 23:09:16 +0000

    Added member.getDataByUsername()

--- trunk/facepunch/member.lua
+++ trunk/facepunch/member.lua
@@ -116,6 +116,14 @@ local memberNotFoundPattern = "" ..
 "<div class=\"blockrow restore\">Invalid User specified."
 
 -------------------------------------------------------------------------------
+-- memberNotRegisteredPattern
+-- Purpose: Pattern for detecting if you specified an unregistered username in
+--			getDataByUsername
+-------------------------------------------------------------------------------
+local memberNotRegisteredPattern = "" ..
+"<div class=\"blockrow restore\">This user has not registered and therefore does not have a profile to view."
+
+-------------------------------------------------------------------------------
 -- memberProfilePattern
 -- Purpose: Pattern for filling a member object from their profile page.
 -------------------------------------------------------------------------------
@@ -144,6 +152,59 @@ function getDataByUserID( userID )
 	local r, c = http.get( facepunch.rootURL .. "/members/" .. userID )
 	if ( c == 200 ) then
 		if string.find( r, memberNotFoundPattern ) then
+			return 2, nil
+		end
+		local member = new()
+		for username,
+			status,
+			displayedUsername,
+			optTitle,
+			joinDate,
+			postCount in string.gmatch( r, memberProfilePattern ) do
+				member.userID			= userID -- Gran PC; uh
+				member.username			= username
+				member.online			= status == "on"
+				member.usertitle		= string.match( optTitle, "<span class=\"usertitle\">(.-)</span>" )
+				if ( member.usertitle ) then
+					member.usertitle	= string.gsub( member.usertitle, "^%s*(.-)%s*$", "%1" )
+				end
+				member.joinDate			= joinDate
+				member.postCount		= postCount
+				member.postCount		= string.gsub( member.postCount, "^%s*(.-)%s*$", "%1" )
+				member.postCount		= tonumber( string.gsub( member.postCount, ",", "" ), 10 )
+
+				member.avatar			= facepunch.rootURL .. "/image.php?u=" .. userID .. "&dateline=" .. os.time() -- fabricated!
+
+				if ( username == displayedUsername ) then
+					member.usergroup	= "Registered User"
+				elseif ( string.find( displayedUsername, "<font color=\"red\">" ) ) then
+					member.usergroup	= "Banned"
+				elseif ( string.find( displayedUsername, "#A06000" ) ) then
+					member.usergroup	= "Gold Member"
+				elseif ( string.find( displayedUsername, "#00aa00" ) ) then
+					member.usergroup	= "Moderator"
+				elseif ( string.find( displayedUsername, "<span class=\"boing\">") ) then
+					member.usergroup	= "Administrator"
+				end
+		end
+		return 0, member
+	else
+		return 1, nil
+	end
+end
+
+-------------------------------------------------------------------------------
+-- member.getDataByUsername()
+-- Purpose: Fills a member structure from a member username.
+-- Input: username - user's username
+--		  session - optional session object
+-- Output: error code, member object
+-- Error Codes: 2 - user not found
+-------------------------------------------------------------------------------
+function getDataByUsername( username )
+	local r, c = http.get( facepunch.rootURL .. "/member.php?username=" .. username )
+	if ( c == 200 ) then
+		if string.find( r, memberNotRegisteredPattern ) then
 			return 2, nil
 		end
 		local member = new()
